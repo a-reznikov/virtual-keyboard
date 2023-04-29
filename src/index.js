a8df571c69afde2552df47318130f0ad64f606e5
a-reznikov <a-reznnikov@gmail.com>
2023-04-29 19:32:02 +0300

    feat: add rule import/export to eslintrc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import { buttons } from './js/buttons';
+
+buttons('John');
 console.log('Start task');
---
+++ src/js/buttons.js
@@ -0,0 +1,4 @@
+export function buttons() {
+  // eslint-disable-next-line no-alert
+  alert('Buttons export');
+}
